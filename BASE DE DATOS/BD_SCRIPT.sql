5a1466e3436fea481c1a23e765d961832a821805
Warias30360 <warias30360@ufide.ac.cr>
2024-06-28 20:00:11 -0600

    NewSPDisableUserAdmin
    Des_activacionUsuario

--- BASE DE DATOS/BD_SCRIPT.sql
+++ BASE DE DATOS/BD_SCRIPT.sql
@@ -3344,3 +3344,34 @@ BEGIN
        FROM CARGO;
 END
 GO
+
+-- =======================================================
+-- Create Stored Procedure Template for Azure SQL Database
+-- =======================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:      My Name
+-- Create Date: 06/28/2024
+-- Description: Cambia el estado de un empleado a false (Inactivo).
+-- =============================================
+CREATE PROCEDURE Des_activacionUsuario
+(
+    -- Add the parameters for the stored procedure here
+    @id_empleado bigint
+)
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON
+
+    -- Insert statements for procedure here
+    UPDATE EMPLEADO
+	SET
+		ESTADO = 'FALSE'
+	WHERE ID_EMPLEADO = @id_empleado;
+END
+GO
