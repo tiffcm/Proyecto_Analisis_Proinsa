e4cba129b5633fe7295c20e2c6f4cc2f9c1cfd89
Brandruiz7 <bruiz20932@ufide.ac.cr>
2024-06-28 22:53:48 -0600

    Depurando Errores
    - El dataTable queda funcional.
    - Se soluciona el error en crear solicitud cambio de horario y vacaciones

--- PROINSA_GP_WEB/PROINSA_GP_WEB/Entidad/Solicitud.cs
+++ PROINSA_GP_WEB/PROINSA_GP_WEB/Entidad/Solicitud.cs
@@ -3,38 +3,22 @@
     public class Solicitud
     {
 
-        
-        
+        public long ID { get; set; }
+        public string? IDENTIFICACION { get; set; }
+        public long? SOLICITANTE_ID { get; set; }
+        public string? NOMBRECOMPLETO { get; set; }
+        public string? COMENTARIO { get; set; }
+        public string? DETALLE { get; set; }
+        public string? DESCRIPCION { get; set; }
+        public long? TIPOSOLICITUD_ID { get; set; }
+        public string? NOMBRE_TIPO_SOLICITUD { get; set; }
+        public string? ESTADO_SOLICITUD { get; set; }
+        public long ID_HORARIOLABORAL { get; set; }
+        public string? HORARIO { get; set; }
+        public string? ESTADO { get; set; }
+        public int DIAS { get; set; }
+        public DateTime FECHA_SOLICITUD { get; set; }
         public DateTime? FECHA_INICIO { get; set; }
         public DateTime? FECHA_FINAL { get; set; }
-
-        public string? COMENTARIO { get; set; }
-
-        public string? DETALLE { get; set; }
-
-       
-
-        public long? SOLICITANTE_ID { get; set; }
-
-        public long? TIPOSOLICITUD_ID { get; set; }
-
-        public long ID { get; set; }
-        public string? DESCRIPCION { get; set; }
-
-        public DateTime FECHA_SOLICITUD { get; set; }
-
-        public string? NOMBRECOMPLETO { get; set; }
-
-        public string? IDENTIFICACION { get; set; }
-
-        public string? NOMBRE_TIPO_SOLICITUD { get; set; }
-
-
-        public string? ESTADO { get; set; }
-
-        public string? HORARIO { get; set; }
-
-        public long ID_HORARIOLABORAL { get; set; }
-        
     }
 }
