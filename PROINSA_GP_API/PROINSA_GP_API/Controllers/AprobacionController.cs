728da9f0fd10e9abd3484e0e12088f71272bcf2b
tifannyc <136260029+tiffcm@users.noreply.github.com>
2024-06-29 22:21:29 -0600

    Aprobación de solicitudes
    Se agregan los procedimientos en la base de datos , se crean endpoints en el api.

---
+++ PROINSA_GP_API/PROINSA_GP_API/Controllers/AprobacionController.cs
@@ -0,0 +1,113 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using PROINSA_GP_API.Entidad;
+using System.Data;
+
+namespace PROINSA_GP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AprobacionController(IConfiguration iConfiguration) : ControllerBase
+    {
+
+
+        [HttpGet]
+        [Route("ObtenerAprobacionPendiente")]
+        public async Task<IActionResult> ObtenerAprobacionPendiente(long id_empleado)
+        {
+            Respuesta respuesta = new Respuesta();
+
+            using (var contexto = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:Db_Connection").Value))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@id_empleado", id_empleado);
+
+                var request = (await contexto.QueryAsync<Aprobacion>("ObtenerAprobacionPendiente", parameters, commandType: System.Data.CommandType.StoredProcedure)).ToList();
+
+                if (request != null && request.Count > 0)
+                {
+                    respuesta.CODIGO = 1;
+                    respuesta.MENSAJE = "OK";
+                    respuesta.CONTENIDO = request;
+                    return Ok(respuesta);
+                }
+                else
+                {
+                    respuesta.CODIGO = 0;
+                    respuesta.MENSAJE = "No hay registros";
+                    return Ok(respuesta);
+                }
+            }
+        }
+
+
+
+        [HttpGet]
+        [Route("ObtenerAprobacionPendienteDetalle")]
+        public async Task<IActionResult> ObtenerAprobacionPendienteDetalle( long ID_SOLICITUD, long id_empleado)
+        {
+            Respuesta respuesta = new Respuesta();
+
+            
+                using (var contexto = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:Db_Connection").Value))
+                {
+                    var parameters = new DynamicParameters();
+                    
+                    parameters.Add("@ID_SOLICITUD", ID_SOLICITUD);
+                    parameters.Add("@id_empleado", id_empleado);
+
+                    var request = (await contexto.QueryAsync<Aprobacion>("ObtenerAprobacionPendienteDetalle", parameters, commandType: System.Data.CommandType.StoredProcedure)).ToList();
+
+                    if (request != null && request.Count > 0)
+                    {
+                        respuesta.CODIGO = 1;
+                        respuesta.MENSAJE = "OK";
+                        respuesta.CONTENIDO = request;
+                        return Ok(respuesta);
+                    }
+                    else
+                    {
+                        respuesta.CODIGO = 0;
+                        respuesta.MENSAJE = "No hay registros";
+                        return Ok(respuesta);
+                    }
+                }
+            }
+
+
+        [HttpGet]
+        [Route("ObtenerAprobacionFlujo")]
+        public async Task<IActionResult> ObtenerAprobacionFlujo(long ID_SOLICITUD)
+        {
+            Respuesta respuesta = new Respuesta();
+
+
+            using (var contexto = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:Db_Connection").Value))
+            {
+                var parameters = new DynamicParameters();
+
+                parameters.Add("@ID_SOLICITUD", ID_SOLICITUD);
+
+                var request = (await contexto.QueryAsync<Aprobacion>("ObtenerAprobacionFlujo", parameters, commandType: System.Data.CommandType.StoredProcedure)).ToList();
+
+                if (request != null && request.Count > 0)
+                {
+                    respuesta.CODIGO = 1;
+                    respuesta.MENSAJE = "OK";
+                    respuesta.CONTENIDO = request;
+                    return Ok(respuesta);
+                }
+                else
+                {
+                    respuesta.CODIGO = 0;
+                    respuesta.MENSAJE = "No hay registros";
+                    return Ok(respuesta);
+                }
+            }
+        }
+
+    }
+
+    }
+
---
+++ PROINSA_GP_API/PROINSA_GP_API/Entidad/Aprobacion.cs
@@ -0,0 +1,39 @@
+﻿namespace PROINSA_GP_API.Entidad
+{
+    public class Aprobacion
+    {
+
+        public long ID_SOLICITUD { get; set; }
+
+        public DateTime FECHA_SOLICITUD { get; set; }
+        public string? SOLICITANTE { get; set; }
+
+        public string? IDENTIFICACION { get; set; }
+
+        public string? TIPOPERMISO { get; set; }
+        public string? HORARIO { get; set; }
+        public int DIAS { get; set; }
+
+        public string? RESPUESTASOLICITUD { get; set; }
+
+        public DateTime FECHAINICIO { get; set; }
+
+        public DateTime FECHAFINAL { get; set; }
+
+        public string? COMENTARIO { get; set; }
+
+        public string? DETALLE { get; set; }
+
+        public string? HORARIONUEVO { get; set; }
+
+        public string? HORARIOACTUAL { get; set; }
+
+        public string? DEPARTAMENTO { get; set; }
+
+        public string? NOMBRECOMPLETO { get; set; }
+
+        public int SECUENCIA { get; set; }
+
+
+    }
+}
