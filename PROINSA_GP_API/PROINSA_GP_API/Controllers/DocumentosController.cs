6a201c8f390966695a9c5514f7412e518aa99065
Brandruiz7 <bruiz20932@ufide.ac.cr>
2024-07-24 19:35:58 -0600

    Lógica de actualizar y eliminar documentos
    Se agregan SP y queda funcional en el API

--- PROINSA_GP_API/PROINSA_GP_API/Controllers/DocumentosController.cs
+++ PROINSA_GP_API/PROINSA_GP_API/Controllers/DocumentosController.cs
@@ -26,7 +26,7 @@ namespace PROINSA_GP_API.Controllers
                 parametros.Add("@EMPLEADO_ID", entidad.EMPLEADO_ID);
                 parametros.Add("@DOCUMENTO", entidad.DOCUMENTO);
 
-                var result = await context.ExecuteAsync("REGISTRAR_DOCUMENTO", parametros ,
+                var result = await context.ExecuteAsync("Registrar_Documento", parametros ,
                 commandType: CommandType.StoredProcedure);
 
                 if (result > 0)
@@ -56,7 +56,7 @@ namespace PROINSA_GP_API.Controllers
                 var parameters = new DynamicParameters();
                 parameters.Add("@EMPLEADO_ID", EMPLEADO_ID);
 
-                var request = (await contexto.QueryAsync<Documento>("CONSULTAR_DOCUMENTOEMPLEADO", parameters,
+                var request = (await contexto.QueryAsync<Documento>("Consultar_DocumentoEmpleado", parameters,
                     commandType: System.Data.CommandType.StoredProcedure)).ToList();
 
                 if (request != null && request.Count > 0)
@@ -81,7 +81,7 @@ namespace PROINSA_GP_API.Controllers
             Respuesta respuesta = new Respuesta();
             using (var contexto = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:Db_Connection").Value))
             {
-                var request = await contexto.QueryAsync<SelectListItem>("CONSULTAR_TIPOSDOCUMENTO",
+                var request = await contexto.QueryAsync<SelectListItem>("Consultar_TiposDocumento",
                     new { },
                     commandType: System.Data.CommandType.StoredProcedure);
                 if (request != null)
@@ -95,6 +95,80 @@ namespace PROINSA_GP_API.Controllers
                 {
                     respuesta.CODIGO = 0;
                     respuesta.MENSAJE = "No hay tipos de documento registrados";
+                    respuesta.CONTENIDO = false;
+                    return Ok(respuesta);
+                }
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarDocumento")]
+        public async Task<IActionResult> ActualizarDocumento(Documento entidad)
+        {
+            Respuesta respuesta = new Respuesta();
+
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:Db_Connection").Value))
+            {
+                var parametros = new DynamicParameters();
+                parametros.Add("@ID_EMPLEADODOCUMENTO", entidad.ID_EMPLEADODOCUMENTO);
+                parametros.Add("@NOMBRE_DOCUMENTO", entidad.NOMBRE_DOCUMENTO);
+                parametros.Add("@COMENTARIO", entidad.COMENTARIO);
+                if (entidad.DOCUMENTO != null)
+                {
+                    parametros.Add("@DOCUMENTO", entidad.DOCUMENTO);
+                }
+                else
+                {
+                    byte[] bytes = new byte[] { 0x4E };
+                    parametros.Add("@DOCUMENTO", bytes);
+                }                
+                parametros.Add("@TIPODOCUMENTO_ID", entidad.TIPODOCUMENTO_ID);
+
+                var result = await context.ExecuteAsync("ActualizarDocumento", parametros,
+                commandType: CommandType.StoredProcedure);
+
+                if (result > 0)
+                {
+                    respuesta.CODIGO = 1;
+                    respuesta.MENSAJE = "OK";
+                    respuesta.CONTENIDO = true;
+                    return Ok(respuesta);
+                }
+                else
+                {
+                    respuesta.CONTENIDO = 0;
+                    respuesta.MENSAJE = "No puede actualizar ese documento debido a un error.";
+                    respuesta.CONTENIDO = false;
+                    return Ok(respuesta);
+                }
+            }
+        }
+
+        [HttpDelete]
+        [Route("EliminarDocumento")]
+        public async Task<IActionResult> EliminarDocumento(long ID_EMPLEADODOCUMENTO)
+        {
+            Respuesta respuesta = new Respuesta();
+
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:Db_Connection").Value))
+            {
+                var parametros = new DynamicParameters();
+                parametros.Add("@ID_EMPLEADODOCUMENTO", ID_EMPLEADODOCUMENTO);
+
+                var result = await context.ExecuteAsync("EliminarDocumento", parametros,
+                commandType: CommandType.StoredProcedure);
+
+                if (result > 0)
+                {
+                    respuesta.CODIGO = 1;
+                    respuesta.MENSAJE = "OK";
+                    respuesta.CONTENIDO = true;
+                    return Ok(respuesta);
+                }
+                else
+                {
+                    respuesta.CONTENIDO = 0;
+                    respuesta.MENSAJE = "No puede eliminar ese documento debido a un error.";
                     respuesta.CONTENIDO = false;
                     return Ok(respuesta);
                 }
