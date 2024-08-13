8b7935cea57f6ca7948e426f93b477d43cf3f79e
Brandruiz7 <bruiz20932@ufide.ac.cr>
2024-08-12 22:39:07 -0600

    Vistas Nómina y ajustes DB
    - Se crean algunas vistas preliminares para nómina.
    - Se arreglan detalles de la base de datos

--- PROINSA_GP_WEB/PROINSA_GP_WEB/wwwroot/js/dataTable.js
+++ PROINSA_GP_WEB/PROINSA_GP_WEB/wwwroot/js/dataTable.js
@@ -8,3 +8,29 @@
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var dataTable2 = document.getElementById('dataTableDeducciones');
+    if (dataTable2) {
+        var dataTableInstance = new DataTable(dataTable2, {
+            language: {
+                url: '//cdn.datatables.net/plug-ins/2.0.5/i18n/es-ES.json'
+            },
+            "paging": false,
+            "bInfo": false
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var dataTable2 = document.getElementById('dataTableIngresos');
+    if (dataTable2) {
+        var dataTableInstance = new DataTable(dataTable2, {
+            language: {
+                url: '//cdn.datatables.net/plug-ins/2.0.5/i18n/es-ES.json'
+            },
+            "paging": false,
+            "bInfo": false
+        });
+    }
+});
