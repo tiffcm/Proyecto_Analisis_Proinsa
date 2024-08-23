a39ee2627239c9456c9b3d6bab9c90f655292db5
tifannyc <136260029+tiffcm@users.noreply.github.com>
2024-08-22 21:16:53 -0600

    Ejemplo descarga de reporte

---
+++ PROINSA_GP_WEB/PROINSA_GP_WEB/Controllers/ReportController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace PROINSA_GP_WEB.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public ReportController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> DownloadReportExcel(string reportName = "ReporteEmpleados")
+        {
+
+
+
+            var reportUrl = $"http://tc-hp-cnd2016fn/ReportServer?/GestionPersonal/{reportName}&rs:Command=Render&rs:Format=EXCELOPENXML";
+            //url del servidor de reportes 
+
+            var handler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential("", "", "")
+
+                // colocar aca nombre de usuario , contrasenna y dominio de la maquina
+            };
+
+
+            using (var client = new HttpClient(handler))
+            {
+                try
+                {
+
+                    var response = await client.GetAsync(reportUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        var content = await response.Content.ReadAsByteArrayAsync();
+
+                        return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ReporteEmpleados.xlsx");
+                    }
+                    else
+                    {
+
+                        var errorContent = await response.Content.ReadAsStringAsync();
+                        return Content($"Error al obtener el reporte: {response.StatusCode} - {errorContent}");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    return Content($"Error al intentar obtener el reporte: {ex.Message}");
+                }
+            }
+
+
+
+
+
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> DownloadReportPdf(string reportName = "ReporteEmpleados")
+        {
+
+            var reportUrl = $"http://tc-hp-cnd2016fn/ReportServer?/GestionPersonal/{reportName}&rs:Command=Render&rs:Format=PDF";
+
+
+            var handler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential("", "", "")
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                try
+                {
+
+                    var response = await client.GetAsync(reportUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        var content = await response.Content.ReadAsByteArrayAsync();
+
+                        return File(content, "application/pdf", $"{reportName}.pdf");
+                    }
+                    else
+                    {
+
+                        var errorContent = await response.Content.ReadAsStringAsync();
+                        return Content($"Error al obtener el reporte: {response.StatusCode} - {errorContent}");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    return Content($"Error al intentar obtener el reporte: {ex.Message}");
+                }
+            }
+        }
+
+
+    }
+}
+
