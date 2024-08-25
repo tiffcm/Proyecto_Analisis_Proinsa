2726fd37756fc1aed859fef3948ff71313051260
Brandon Ruiz <bruiz20932@ufide.ac.cr>
2024-08-25 17:25:15 -0600

    Solución tema reportes

--- PROINSA_GP_WEB/PROINSA_GP_WEB/Controllers/ReportesController.cs
+++ PROINSA_GP_WEB/PROINSA_GP_WEB/Controllers/ReportesController.cs
@@ -1,49 +1,100 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using P_WebMartes.Models;
+using PROINSA_GP_WEB.Entidad;
 using PROINSA_GP_WEB.Models;
+using PROINSA_GP_WEB.Servicios;
+using System.Net;
 
 namespace PROINSA_GP_WEB.Controllers
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ReportesController : Controller
-    {
+    public class ReportesController(IReporteModel _iReporteModel) : Controller
+    {        
         [Seguridad]
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult DownloadReport()
         {
+
+            long? ID_EMPLEADO = HttpContext.Session.GetInt32("ID_EMPLEADO");
+            if (ID_EMPLEADO == null)
+            {
+                return RedirectToAction("Login"); // Redirige a login si el usuario no está autenticado
+            }
+
+            /// Se deben llamar los SPs para las vistas de reportes
+
             return View();
         }
 
-        [Seguridad]
-        [Administrador]
         [HttpGet]
-        public IActionResult AgregarReporte()
+        public async Task<IActionResult> DownloadReportExcel(string reportName)
         {
-            return View();
+            var reportUrl = $"http://tc-hp-cnd2016fn/ReportServer?/GestionPersonal/{reportName}&rs:Command=Render&rs:Format=EXCELOPENXML";
+            //url del servidor de reportes 
+
+            var handler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential("", "", "")
+
+                // colocar aca nombre de usuario , contrasenna y dominio de la maquina
+            };
+
+
+            using (var client = new HttpClient(handler))
+            {
+                try
+                {
+                    var response = await client.GetAsync(reportUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsByteArrayAsync();
+                        return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ReporteEmpleados.xlsx");
+                    }
+                    else
+                    {
+                        var errorContent = await response.Content.ReadAsStringAsync();
+                        return Content($"Error al obtener el reporte: {response.StatusCode} - {errorContent}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Content($"Error al intentar obtener el reporte: {ex.Message}");
+                }
+            }
         }
 
-        [Seguridad]
-        [Administrador]
         [HttpGet]
-        public IActionResult ModificarReporte()
+        public async Task<IActionResult> DownloadReportPdf(string reportName)
         {
-            return View();
+            var reportUrl = $"http://tc-hp-cnd2016fn/ReportServer?/GestionPersonal/{reportName}&rs:Command=Render&rs:Format=PDF";
+
+            var handler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential("", "", "")
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                try
+                {
+                    var response = await client.GetAsync(reportUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsByteArrayAsync();
+                        return File(content, "application/pdf", $"{reportName}.pdf");
+                    }
+                    else
+                    {
+                        var errorContent = await response.Content.ReadAsStringAsync();
+                        return Content($"Error al obtener el reporte: {response.StatusCode} - {errorContent}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Content($"Error al intentar obtener el reporte: {ex.Message}");
+                }
+            }
         }
 
-        [Seguridad]
-        [Administrador]
-        [HttpGet]
-        public IActionResult DetallarReporte()
-        {
-            return View();
-        }
-
-        [Seguridad]
-        // [Administrador]
-        [HttpGet]
-        public IActionResult EliminarReporte()
-        {
-            return View();
-        }
+        ///// SPs para la vista
+        ///
     }
 }
--- PROINSA_GP_WEB/PROINSA_GP_WEB/Program.cs
+++ PROINSA_GP_WEB/PROINSA_GP_WEB/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddSingleton<IAprobacionModel, AprobacionModel>();
 builder.Services.AddSingleton<IDocumentoModel, DocumentoModel>();
 builder.Services.AddSingleton<IActividadModel, ActividadModel>();
 builder.Services.AddSingleton<INominaModel, NominaModel>();
-builder.Services.AddSingleton<IReportModel, ReportModel>();
+builder.Services.AddSingleton<IReporteModel, ReporteModel>();
 
 // Para autenticación con AZURE
 builder.Services.AddAuthentication().AddMicrosoftAccount(opciones =>
