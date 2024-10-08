a83b495df6a09c0fed0795ac85094effaf7b43f8
tifannyc <136260029+tiffcm@users.noreply.github.com>
2024-10-08 16:37:53 -0600

    Update Program.cs

--- PROINSA_GP_API/PROINSA_GP_API/Program.cs
+++ PROINSA_GP_API/PROINSA_GP_API/Program.cs
@@ -9,12 +9,20 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("./swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 //Se agrega la captura de los errores.
